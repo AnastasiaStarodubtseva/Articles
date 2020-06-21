33b1ffc7957e95c468d6482ad482cfbffd543948
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-06-22 00:10:22 +0700

    Load articles from API

--- src/Main.elm
+++ src/Main.elm
@@ -10,35 +10,68 @@ import Html.Attributes exposing (class)
 type alias Article =
   { title : String
   , content : String
+  , userId : Int
+  }
+
+type alias Author =
+  { name : String
+  , id : Int
   }
 
 type alias Model =
   { articles : List Article
+  , authors : List Author
   }
 
 type alias Flags = {}
 
 type Msg
   = FetchArticlesDone (Result Http.Error (List Article))
-  
+  | FetchAuthorsDone (Result Http.Error (List Author))
+
 decodeArticle : Decoder Article
-decodeArticle = 
-  map2 Article 
+decodeArticle =
+  map3 Article
     (field "title" string)
     (field "body" string)
+    (field "userId" int)
+
 
 articlesDecoder : Decoder (List Article)
 articlesDecoder = list decodeArticle
 
-fetchArticles : Cmd Msg 
-fetchArticles = 
-  Http.get 
+fetchArticles : Cmd Msg
+fetchArticles =
+  Http.get
     { url = "https://jsonplaceholder.typicode.com/posts"
     , expect = Http.expectJson FetchArticlesDone articlesDecoder
     }
+decodeAuthor : Decoder Author
+decodeAuthor =
+  map2 Author
+    (field "name" string)
+    (field "id" int)
+
+authorsDecoder : Decoder (List Author)
+authorsDecoder = list decodeAuthor
+
+
+
+
+fetchAuthors : Cmd Msg
+fetchAuthors =
+  Http.get
+    { url = "https://jsonplaceholder.typicode.com/users"
+    , expect = Http.expectJson FetchAuthorsDone authorsDecoder
+    }
 
 init : Flags -> (Model, Cmd Msg)
-init flags = ({ articles = [] }, fetchArticles)
+init flags =
+  ( { articles = []
+    , authors = []
+    }
+  , Cmd.batch [ fetchArticles, fetchAuthors ]
+  )
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
@@ -49,21 +82,38 @@ update msg model = case msg of
          in (model, Cmd.none)
       Ok newArticles ->
         ({ model | articles = newArticles }, Cmd.none)
+  FetchAuthorsDone result ->
+    case result of
+      Err _ ->
+        let _ = Debug.log "result" result
+          in (model, Cmd.none)
+      Ok newAuthors ->
+        ({ model | authors = newAuthors }, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions _ = Sub.batch []
 
-articleRowView : List Article -> Html a
-articleRowView articles = 
-  div [ class "row" ] (List.map articleView articles)
+articleRowView : Model -> List Article -> Html a
+articleRowView model articles =
+  div [ class "row" ] (List.map (articleView model) articles)
 
-articleView : Article -> Html a
-articleView article =
-  div
-    [ class "article"
-    , class "four"
-    , class "columns"
-    ] [text article.title]
+articleView : Model -> Article -> Html a
+articleView model article =
+  let
+      mAuthor =
+        List.head (List.filter (\a -> a.id == article.userId) model.authors)
+  in
+      div
+        [ class "article"
+        , class "four"
+        , class "columns"
+        ] [ p [class "title"] [text article.title]
+          , p [class "content"] [text article.content]
+          , case mAuthor of 
+              Nothing ->  text ""
+              Just author -> 
+                p [class "author"] [text author.name]
+          ]
 
 view : Model -> Html a
 view model =
@@ -71,7 +121,7 @@ view model =
       rows = groupsOf 3 model.articles
   in
       div []
-        [ div [ class "container" ] (List.map articleRowView rows)
+        [ div [ class "container" ] (List.map (articleRowView model) rows)
         ]
 
 main : Program Flags Model Msg
